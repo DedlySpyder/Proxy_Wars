d36e571e8a40fee4f67b6acc33552732a57c1000
DedlySpyder <dedlyspyder@gmail.com>
2020-05-01 19:53:58 -0400

    0.0.10 Fixed crash on Factorio boot due to change in custom-inputs

--- prototypes/hotkeys.lua
+++ prototypes/hotkeys.lua
@@ -2,31 +2,26 @@ data:extend({
 	{
 		type = "custom-input",
 		name = "Proxy_Wars_view_scoreboard",
-		key_sequence = "Tab",
-		consuming = "script-only"
+		key_sequence = "Tab"
 	},
 	{
 		type = "custom-input",
 		name = "Proxy_Wars_view_value_list",
-		key_sequence = "F1",
-		consuming = "script-only"
+		key_sequence = "F1"
 	},
 	{
 		type = "custom-input",
 		name = "Proxy_Wars_view_buy_biters",
-		key_sequence = "F3",
-		consuming = "script-only"
+		key_sequence = "F3"
 	},
 	{
 		type = "custom-input",
 		name = "Proxy_Wars_buy_biter_modifier",
-		key_sequence = "F2",
-		consuming = "script-only"
+		key_sequence = "F2"
 	},
 	{
 		type = "custom-input",
 		name = "Proxy_Wars_view_help",
-		key_sequence = "H",
-		consuming = "script-only"
+		key_sequence = "H"
 	}
 })
